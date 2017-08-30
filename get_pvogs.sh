3b2f473e779883ccb73457622907a22615405de7
gmteunisse <gmteunisse@gmail.com>
2017-08-30 00:45:39 -0300

    Update get_pvogs.sh

--- get_pvogs.sh
+++ get_pvogs.sh
@@ -4,9 +4,9 @@
 
 echo Downloading pVOGs database...
 python scripts/1_get_pVOGs.py
-python 2_get_sequences.py
+python scripts/2_get_sequences.py
 echo Creating BLAST database...
-python 3_make_blast_db.sh
+python scripts/3_make_blast_db.sh
 echo Creating HMMer database...
-python 4_create_hmmer_db.sh
+python scripts/4_create_hmmer_db.sh
 echo Finished creating database.
