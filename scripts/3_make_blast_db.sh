28d0e1a8787bcb170f1be3f40317a741f9f795ba
gmteunisse <gmteunisse@gmail.com>
2017-11-17 17:27:22 -0300

    Delete 3_make_blast_db.sh

--- scripts/3_make_blast_db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-cd db/pVOGs/
-mkdir blast_db
-
-cd sequences
-cat VOG* > all_sequences.fasta
-cd ..
-makeblastdb -in sequences/all_sequences.fasta -title pVOGs -out blast_db/pVOGs_db -dbtype prot
